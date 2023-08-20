69960ebac0602eb6e12afabfd7039619606e75c7
tomo-local <shinsi4545@gmail.com>
2023-08-21 02:25:29 +0900

    fix badge

--- src/components/common/layouts/header/NotificationMenu.tsx
+++ src/components/common/layouts/header/NotificationMenu.tsx
@@ -6,7 +6,12 @@ import IconMeneBase from '@/components/common/layouts/header/IconMeneBase'
 export default function NotificationMenu() {
   return (
     <div className="w-12">
-      <Badge withBorder className="border-sub top-3 right-2.5" content="">
+      <Badge
+        withBorder
+        className="border-sub top-3 right-2.5"
+        content=""
+        invisible
+      >
         <IconMeneBase icon={<BellIcon className="w-6 h-6" />}>
           notification
         </IconMeneBase>
